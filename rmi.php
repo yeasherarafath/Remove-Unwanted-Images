34675bf56a9bc72456f4a1624db3a572a9ef7482
Yasir Arafat <yeasherarafath@gmail.com>
2025-11-02 18:03:30 +0600

    first commit

---
+++ rmi.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class rmi extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rmi';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove unwanted images from assets path';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $sqlFileContent = file_get_contents(base_path('DB/escrova.sql'));
+
+        $allImages = File::allFiles(public_path('global/uploads'));
+
+        foreach ($allImages as $key => $file) {
+            $fileName = basename($file);
+            $mimeType = File::mimeType($file);
+
+            if (!str_contains($sqlFileContent, $fileName) && str_contains($mimeType, 'image')) {
+                File::delete(public_path($file));
+                $this->info("Deleted: $fileName");
+            }
+        }
+    }
+}
